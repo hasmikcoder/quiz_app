dca466a01bd601b0c9c144db06addd789ad448ec
unknown <hasvart@gmail.com>
2023-02-01 17:41:00 -0600

    Added an array with questions

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-const quizData=[   ];
+const quizData=[  
+    {
+    question: "What is the most used programming language in 2019?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+},
+{
+    question: "Who is the President of US?",
+    a: "Florin Pop",
+    b: "Donald Trump",
+    c: "Ivan Saldano",
+    d: "Mihai Andrei",
+    correct: "b",
+},
+{
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a",
+},
+{
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "b",
+},
+];
 
 const a_text=document.getElementbById('a_text');
 const b_text=document.getElementbById('b_text');
@@ -11,10 +44,10 @@ let currentQuestion=0;
 loadQuiz();
 
 function loadQuiz(){
-    const currentQuiz=quizData[currentquestion];
+    const currentQuizData=quizData[currentquestion];
     questionEl.innerHTML=quizData[currentQuestion]
 
-    
+
     currentQuestion++;
 
 };
